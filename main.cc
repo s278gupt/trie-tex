5bd38396e66ebd39cc00d7315f82d3783cc6a8ed
s278gupt <s278gupt@edu.uwaterloo.ca>
2020-11-06 11:38:58 +0300

    user given fname

--- main.cc
+++ main.cc
@@ -6,11 +6,10 @@
 #include "trie.h"
 using namespace std;
 
-void printLatex(Trie *t) {
+void printLatex(Trie *t, string fname) {
    ofstream file;
-   string filename = "example.txt";
-   file.open(filename);
-   cout << "Writing to " << filename << endl;
+   file.open(fname);
+   cout << "Writing to " << fname << endl;
    
    file << "\\begin{center}" << endl;
    file << "\\begin{tikzpicture}[" << endl;
@@ -35,6 +34,12 @@ void printLatex(Trie *t) {
 int main() {
    Trie *t = new Trie;
 
+   // Filename to save tex
+   string filename = "trie_tex.txt";
+   cout << "Enter filename to save latex in (default is trie_tex.txt):";
+   getline(cin, filename);
+   cout << "Latex trie will be saved as " << filename << " in the current directory." << endl;
+
    string line;
    string cmd;
    int numChildren;
@@ -48,7 +53,7 @@ int main() {
       t->root = createNode(NULL, "", numChildren, "");
       cout << "Added root successfully" << endl;
    } else {
-      cout << "Input must start with root command. See `trie-tex help` for more." << endl;
+      cout << "Input must start with root command. See github documentation for more." << endl;
       return 1;
    }
 
@@ -56,7 +61,7 @@ int main() {
    int success = createTrie(t->root);
    if (success == 0) {
       // Print Latex
-      printLatex(t);
+      printLatex(t, filename);
    }
 
    deleteTrie(t);
